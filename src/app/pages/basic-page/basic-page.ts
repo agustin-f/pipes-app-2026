fdb578f00530fbcc3fb6a493e1515d087870d1df
Agustin Farisano <afarisano@bitbucket.org>
2026-02-26 08:43:15 -0300

    agrego pipes

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.html',
   styleUrl: './basic-page.css',
 })
-export default class BasicPage {}
+export default class BasicPage {
+  nameLower = signal('agustin');
+  nameUpper = signal('AGUSTIN');
+  fullName = signal('AgUSTin FArIsAnO');
+}
