8a3664292072e76a01a10228589c2a4a69106428
Agustin Farisano <afarisano@bitbucket.org>
2026-02-27 13:07:46 -0300

    agrego pipes numericos

--- src/app/pages/number-page/number-page.ts
+++ src/app/pages/number-page/number-page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './number-page.html',
   styleUrl: './number-page.css',
 })
-export default class NumberPage {}
+export default class NumberPage {
+  totalSells = signal(2434444444.57676);
+  percent = signal(0.4892);
+}
