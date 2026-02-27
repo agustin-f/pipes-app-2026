b05cbf5ee51819d87920a11805e18913b84728ac
Agustin Farisano <afarisano@bitbucket.org>
2026-02-27 13:30:28 -0300

    agrego pipes de fechas

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,10 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
   styleUrl: './basic-page.css',
 })
@@ -11,4 +12,17 @@ export default class BasicPage {
   nameLower = signal('agustin');
   nameUpper = signal('AGUSTIN');
   fullName = signal('AgUSTin FArIsAnO');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
 }
